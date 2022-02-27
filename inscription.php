ae72d6fcef531a66eb7516e632288ead2b33788a
Julien Castillo <juliencastillo77@gmail.com>
2022-02-27 19:10:09 +0100

    Add field pseudo

--- inscription.php
+++ inscription.php
@@ -1,96 +1,90 @@
 <?php
-	
-	session_start();
 
-	require('src/log.php');
-	
-	
+session_start();
 
-	if(!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['password_two'])){
+require('src/log.php');
 
-		require('src/connect.php');
 
-		// VARIABLES
-		$email 				= htmlspecialchars($_POST['email']);
-		$password 			= htmlspecialchars($_POST['password']);
-		$password_two		= htmlspecialchars($_POST['password_two']);
 
-		// PASSWORD = PASSWORD TWO
-		if($password != $password_two){
+if (!empty($_POST['email']) && !empty($_POST['pseudo']) && !empty($_POST['password']) && !empty($_POST['password_two'])) {
 
-			header('location: inscription.php?error=1&message=Vos mots de passe ne sont pas identiques.');
-			exit();
+	require('src/connect.php');
 
-		}
+	// VARIABLES
+	$email 				= htmlspecialchars($_POST['email']);
+	$pseudo             = htmlspecialchars(($_POST['pseudo']));
+	$password 			= htmlspecialchars($_POST['password']);
+	$password_two		= htmlspecialchars($_POST['password_two']);
 
-		// ADRESSE EMAIL VALIDE
-		if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+	// PASSWORD = PASSWORD TWO
+	if ($password != $password_two) {
 
-			header('location: inscription.php?error=1&message=Votre adresse email est invalide.');
-			exit();
-
-		}
-
-		// EMAIL DEJA UTILISEE
-		$req = $db->prepare("SELECT count(*) as numberEmail FROM users WHERE email = ?");
-		$req->execute(array($email));
-
-		while($email_verification = $req->fetch()){
-
-			if($email_verification['numberEmail'] != 0){
-
-				header('location: inscription.php?error=1&message=Votre adresse email est déjà utilisée par un autre utilisateur.');
-				exit();
-
-			}
-
-		}
-
-		// HASH
-		$secret = sha1($email).time();
-		$secret = sha1($secret).time();
-
-		// CHIFFRAGE DU MOT DE PASSE
-		$password = "aq1".sha1($password."123")."25";
-
-		// ENVOI
-		$req = $db->prepare("INSERT INTO users(email, password, secret) VALUES(?,?,?)");
-		$req->execute(array($email, $password, $secret));
-
-		header('location: inscription.php?success=1');
+		header('location: inscription.php?error=1&message=Vos mots de passe ne sont pas identiques.');
 		exit();
-
 	}
+
+	// ADRESSE EMAIL VALIDE
+	if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+		header('location: inscription.php?error=1&message=Votre adresse email est invalide.');
+		exit();
+	}
+
+	// EMAIL DEJA UTILISEE
+	$req = $db->prepare("SELECT count(*) as numberEmail FROM users WHERE email = ?");
+	$req->execute(array($email));
+
+	while ($email_verification = $req->fetch()) {
+
+		if ($email_verification['numberEmail'] != 0) {
+
+			header('location: inscription.php?error=1&message=Votre adresse email est déjà utilisée par un autre utilisateur.');
+			exit();
+		}
+	}
+
+	// HASH
+	$secret = sha1($email) . time();
+	$secret = sha1($secret) . time();
+
+	// CHIFFRAGE DU MOT DE PASSE
+	$password = "aq1" . sha1($password . "123") . "25";
+
+	// ENVOI
+	$req = $db->prepare("INSERT INTO users(email, pseudo, password, secret) VALUES(?,?,?,?)");
+	$req->execute(array($email, $pseudo, $password, $secret));
+
+	header('location: inscription.php?success=1');
+	exit();
+}
 
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<title>Netflix</title>
 	<link rel="stylesheet" type="text/css" href="design/default.css">
 	<link rel="icon" type="image/pngn" href="img/favicon.png">
 </head>
+
 <body>
 
 	<?php include('src/header.php'); ?>
-	
+
 	<section>
 		<div id="login-body">
 			<h1>S'inscrire</h1>
 
-			<?php if(isset($_GET['error'])){
+			<?php if (isset($_GET['error'])) {
 
-				if(isset($_GET['message'])) {
+				if (isset($_GET['message'])) {
 
-					echo'<div class="alert error">'.htmlspecialchars($_GET['message']).'</div>';
-
+					echo '<div class="alert error">' . htmlspecialchars($_GET['message']) . '</div>';
 				}
-
-			} else if(isset($_GET['success'])) {
-				
-				echo'<div class="alert success">Vous êtes désormais inscrit. <a href="index.php">Connectez-vous</a>.</div>';
-				
+			} else if (isset($_GET['success'])) {
+				echo '<div class="alert success">Vous êtes désormais inscrit. <a href="index.php">Connectez-vous</a>.</div>';
 			} ?>
 
 			<form method="post" action="inscription.php">
@@ -107,4 +101,5 @@
 
 	<?php include('src/footer.php'); ?>
 </body>
+
 </html>
